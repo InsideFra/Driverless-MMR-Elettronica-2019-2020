68f5d13cc221e2e9a0f396e4e2121324e0b08372
InsideFra <francescophp@gmail.com>
2019-11-25 14:29:57 +0100

    upload files

---
+++ STM32 Files/Centralina 1/Src/RF24.cpp
@@ -0,0 +1,18 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file    RF24.c
+  * @brief   RF24 code and util
+  ******************************************************************************
+  ******************************************************************************
+  Copyright (C) 2011 J. Coliz <maniacbug@ymail.com>
+
+  This program is free software; you can redistribute it and/or
+  modify it under the terms of the GNU General Public License
+  version 2 as published by the Free Software Foundation.
+  ******************************************************************************
+*/
+
+/* USER CODE END Header */
+
+#include "RF24.h"
